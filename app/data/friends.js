ec0f742a0347d3b3037e855a092c998d3dc3673a
Bcalderon400 <briancalderon200@gmail.com>
2019-09-13 13:46:17 -0700

    json array for friends added

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//JSON Array
+
+var friends = [{
+        name: "Tom Hardy",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/TomHardyJuly10_%28cropped%29.jpg/170px-TomHardyJuly10_%28cropped%29.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+    {
+        name: "Paris Hilton",
+        photo: "https://hips.hearstapps.com/vidthumb/images/2019-harpersbazaar-gotobedwithme-ep72-parishilton-ldm-v4-facebook-patty-1567112353.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+    {
+        name: "Daft Punk",
+        photo: "https://www.grammy.com/sites/com/files/styles/image_landscape_hero/public/daftpunk-hero-465500137.jpg?itok=e6bagVVB",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+    {
+        name: "Elmo",
+        photo: "https://mp4media.gannett-cdn.com/28911775001/28911775001_6016699267001_6016692286001-vs.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+    {
+        name: "Pink Ranger",
+        photo: "https://images.halloweencostumes.com/products/24636/1-1/power-rangers-pink-ranger-morphsuit.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+    {
+        name: "John Harrington",
+        photo: "https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU3OTIzNTgwMTQxODQwMDE4/who-invented-the-flush-toilets-featured-photo.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 3, 2, 3]
+    },
+]
+
+module.exports = friends;
