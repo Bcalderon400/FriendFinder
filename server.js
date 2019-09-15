09bd73b5183552b38e37d01829b192de529fd984
Bcalderon400 <briancalderon200@gmail.com>
2019-09-14 17:14:40 -0700

    the thing broke, dont know where exactly, have to debug

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require("express");
 var path = require("path");
-var app = express();
+var bodyParser = require("body-parser")
 
+var app = express();
 var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json)
+app.use(express.static("app/public"));
+
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: "application/vnd.api + json" }));
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
 
 app.listen(PORT, function() {
-    console.log("App is listening on PORT: " + PORT)
+    console.log("Server listening on: http://localhost: " + PORT)
 })
